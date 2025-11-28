e8a28a1da0fc4aca756ed4b13cca712d0e95670a
bitoken01 <125574923@qq.com>
2025-11-28 16:15:16 +0800

    Delete server12-13 directory

--- server12-13/src/routes/userInfoRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const userController = require('../controllers/userInfoController');
-
-router.get('/', userController.getUserInfo);
-router.put('/updata', userController.updataTeammax)
-
-module.exports = router;
